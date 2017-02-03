e535eef360cdfc2209bc5040c7a540f9a8aab7c3
Luke Taylor <luke@deentaylor.com>
2017-02-03 14:55:07 +0000

    Support serving on other ports
    To accomodate development in cloud9

--- app/index.js
+++ app/index.js
@@ -18,4 +18,7 @@ app.get('/profile/:username', (req, res) => {
   if (platform === 'twitter') profilePictures.getTwitterPicture(username, res);
 });
 
-app.listen(3000);
+const args = process.argv.slice(2);
+const port = args.length ? args[0] : 3000
+console.log(`Now listening at 0.0.0.0:${port}`)
+app.listen(port, '0.0.0.0');
