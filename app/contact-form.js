22453d70cfc28af51a5fd00cc7ce9ba9d155c51c
Luke Deen Taylor <luke@deentaylor.com>
2017-02-03 20:58:33 -0500

    HTML email template

--- app/contact-form.js
+++ app/contact-form.js
@@ -1,12 +1,25 @@
+const fs = require('fs');
+const path = require('path');
 const sendmail = require('sendmail')();
 
 
 module.exports.send = (name, emailFrom, message, callback) => {
+  const emailTemplate = fs.readFileSync(path.join(__dirname, './contact-message-template.html'), 'utf8');
+  const htmlMessage = emailTemplate.replace(
+    '[[first]]', name[0]
+  ).replace(
+    '[[last]]', name[1]
+  ).replace(
+    '[[email]]', emailFrom
+  ).replace(
+    '[[message]]', message
+  );
+
   sendmail({
     from: 'contact@arkis.io',
     to: 'luke@deentaylor.com',
     subject: `Arkis contact form: ${name[0]} ${name[1]} (${emailFrom})`,
-    html: message,
+    html: htmlMessage,
   }, (err, reply) => {
     if (err) {
       console.log("ERROR");
