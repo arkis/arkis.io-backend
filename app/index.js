a66d9f8b72c5db38d27987b7408bb63f9592fb17
Luke Deen Taylor <luke@deentaylor.com>
2017-02-04 16:55:24 -0500

    Dummy endpoints for contact form debugging

--- app/index.js
+++ app/index.js
@@ -37,6 +37,12 @@ app.post('/contact', multer().fields([]), (req, res) => {
   contactForm.send(name, emailFrom, message, r => res.send(r));
 });
 
+// Dummy endpoints for contact form (used in debugging)
+// The first endpoint returns 'true' after 1 second, the second endpoint returns 'false'
+app.post('/contact-dummy-true', (req, res) => setTimeout(() => res.send('true'), 1000));
+app.post('/contact-dummy-false', (req, res) => setTimeout(() => res.send('false'), 1000));
+
+
 const args = process.argv.slice(2);
 const port = args.length ? args[0] : 3000;
 console.log(`Now listening at 0.0.0.0:${port}`);
