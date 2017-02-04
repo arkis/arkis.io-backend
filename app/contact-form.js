0d502c17afc67d06cf08ec4a2c8393499ced4876
Luke Deen Taylor <luke@deentaylor.com>
2017-02-03 19:48:45 -0500

    Use local hostnames on Now deployments

--- app/contact-form.js
+++ app/contact-form.js
@@ -1,9 +1,13 @@
+const url = require('url');
 const sendmail = require('sendmail')();
 
 
+const localUrl = process.env.NOW_URL || 'https://arkis.io/';
+const hostname = url.parse(localUrl).hostname;
+
 module.exports.send = (name, emailFrom, message, callback) => {
   sendmail({
-    from: 'contact@arkis.io',
+    from: `contact@${hostname}`,
     to: 'luke@deentaylor.com',
     subject: `Arkis contact form: ${name[0]} ${name[1]} (${emailFrom})`,
     html: message,
